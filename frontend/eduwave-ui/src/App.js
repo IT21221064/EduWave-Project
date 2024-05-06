2a1bedc9b8761f28a356820805593540565ae297
hirudaka <hirudakakumarasingha@gmail.com>
2024-05-07 02:37:20 +0530

    frontend development #1

--- frontend/eduwave-ui/src/App.js
+++ frontend/eduwave-ui/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import EnrollPage from "./pages/EnrollPage/EnrollPage";
+import PaymentPage from "./pages/PaymentPage/PaymentPage";
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/enroll" element={<EnrollPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
        
       </Routes>
     </Router>
---
+++ frontend/eduwave-ui/src/pages/EnrollPage/EnrollPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EnrollPage = () => {
+  return (
+    <div>Enroll Page</div>
+  )
+}
+
+export default EnrollPage
