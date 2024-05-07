7944413c6b0810109da6f8814916dfe5e36fccde
Nadun <it21216046@my.sliit.lk>
2024-05-07 10:56:01 +0530

    authentication and notification services

---
+++ backend/NotificationService/controllers/NotificaationController.js
@@ -0,0 +1,56 @@
+const Notification = require("../models/NotificationModel");
+
+const getNotification = async (req, res) => {
+  try {
+    const notifications = await Notification.find({});
+    res.json(notifications);
+  } catch (error) {
+    res.status(500).json({ message: "Cannot retrieve notifications" });
+  }
+};
+
+const addNotification = async (req, res) => {
+  const {title, description} = req.body;
+  if (!title || !description) {
+    return res.status(400).json({ message: "Fill the inputs" });
+  }
+  try {
+    const notify = await Notification.create({ title, description });
+    res.status(200).json(notify);
+  } catch (error) {
+    res.status(400).json({ message: "" });
+  }
+};
+
+
+
+const deleteNotification = async (req, res) => {
+  const notification = await Notification.findById(req.params.id);
+  if (!notification) {
+    return res.status(404).json({ message: "Notification not found" });
+  }
+  try {
+    await notification.deleteOne();
+    return res.status(200).json({
+      message: "Notification deleted successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getNotificationById = async (req, res) => {
+  const notification = await Notification.findById(req.params.id);
+  if (!notification) {
+    return res.status(404).json({ message: "Notification not found" });
+  }
+  res.status(200).json(notification);
+};
+
+module.exports = {
+  getNotification,
+  addNotification,
+  deleteNotification,
+  getNotificationById,
+};
