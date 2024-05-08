e5202c2c1f5a862d395eb7f09ed033dee5d09ca6
hirudaka <hirudakakumarasingha@gmail.com>
2024-05-08 23:14:40 +0530

    courses and enroll integration #4

--- backend/EnrollService/routes/EnrollRoutes.js
+++ backend/EnrollService/routes/EnrollRoutes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 
-const {addEnroll, deleteEnroll, getEnrolls,getEnrollsByUser} = require("../controllers/EnrollController");
+const {addEnroll, deleteEnroll, getEnrolls,getEnrollsByUser,updateEnroll} = require("../controllers/EnrollController");
 
 
 router.post("/", addEnroll);
 router.delete("/:id", deleteEnroll);
 router.get("/", getEnrolls);
 router.get("/:id", getEnrollsByUser);
+router.put("/:id", updateEnroll);
+
 
 
 module.exports = router;
